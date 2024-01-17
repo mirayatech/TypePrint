1b71aadd77b1020f1941e97a353a01853b3a4cde
〽️iraya <mirayaabrodi@gmail.com>
2024-01-17 19:05:01 +0100

    add: more heading levels

--- src/utilities/parseHeadings.tsx
+++ src/utilities/parseHeadings.tsx
@@ -1,4 +1,7 @@
 export const parseHeadings = (line: string) => {
+  if (line.startsWith("###### ")) return <h6>{line.slice(7)}</h6>;
+  if (line.startsWith("##### ")) return <h5>{line.slice(6)}</h5>;
+  if (line.startsWith("#### ")) return <h4>{line.slice(5)}</h4>;
   if (line.startsWith("### ")) return <h3>{line.slice(4)}</h3>;
   if (line.startsWith("## ")) return <h2>{line.slice(3)}</h2>;
   if (line.startsWith("# ")) return <h1>{line.slice(2)}</h1>;
