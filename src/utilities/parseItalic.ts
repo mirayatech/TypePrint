c7bd88a50ad61a5bfb291d5b3d87c06d62697396
〽️iraya <mirayaabrodi@gmail.com>
2024-01-17 16:54:59 +0100

    change: update from regex to js

--- src/utilities/parseItalic.ts
+++ src/utilities/parseItalic.ts
@@ -1,4 +1,35 @@
 export const parseItalic = (line: string) => {
-  const italicRegex = /(\*|_)(.*?)\1/g;
-  return line.replace(italicRegex, "<em>$2</em>");
+  let newLine = "";
+  let charIndex = 0;
+  let isPotentialItalic = false;
+  let italicText = "";
+  let italicMarker = "";
+
+  while (charIndex < line.length) {
+    if (
+      (line[charIndex] === "*" || line[charIndex] === "_") &&
+      !isPotentialItalic
+    ) {
+      isPotentialItalic = true;
+      italicMarker = line[charIndex];
+      italicText = "";
+      charIndex++;
+    } else if (line[charIndex] === italicMarker && isPotentialItalic) {
+      isPotentialItalic = false;
+      charIndex++;
+      newLine += "<em>" + italicText + "</em>";
+    } else if (isPotentialItalic) {
+      italicText += line[charIndex];
+      charIndex++;
+    } else {
+      newLine += line[charIndex];
+      charIndex++;
+    }
+  }
+
+  if (isPotentialItalic) {
+    newLine += italicMarker + italicText;
+  }
+
+  return newLine;
 };
