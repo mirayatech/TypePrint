11e37221c6534f2e57fc08494718fa9f430e37d9
〽️iraya <mirayaabrodi@gmail.com>
2024-01-17 16:49:29 +0100

    change: update from regex to js

--- src/utilities/parseBold.ts
+++ src/utilities/parseBold.ts
@@ -1,4 +1,40 @@
 export const parseBold = (line: string) => {
-  const boldRegex = /(\*\*|__)(.*?)\1/g;
-  return line.replace(boldRegex, "<strong>$2</strong>");
+  let newLine = "";
+  let charIndex = 0;
+
+  while (charIndex < line.length) {
+    if (
+      (line[charIndex] === "*" && line[charIndex + 1] === "*") ||
+      (line[charIndex] === "_" && line[charIndex + 1] === "_")
+    ) {
+      charIndex += 2;
+      let boldText = "";
+      let endFound = false;
+
+      while (charIndex < line.length) {
+        if (
+          (line[charIndex] === "*" && line[charIndex + 1] === "*") ||
+          (line[charIndex] === "_" && line[charIndex + 1] === "_")
+        ) {
+          charIndex += 2;
+          endFound = true;
+          break;
+        } else {
+          boldText += line[charIndex];
+          charIndex++;
+        }
+      }
+
+      if (endFound) {
+        newLine += `<strong>${boldText}</strong>`;
+      } else {
+        newLine += `**${boldText}`;
+      }
+    } else {
+      newLine += line[charIndex];
+      charIndex++;
+    }
+  }
+
+  return newLine;
 };
