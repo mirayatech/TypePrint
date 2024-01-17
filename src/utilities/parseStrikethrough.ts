e1d5394eaeeb8c15658843cb82810db9960bae05
〽️iraya <mirayaabrodi@gmail.com>
2024-01-17 17:33:09 +0100

    change: update parseStrikethrough from regex to js

--- src/utilities/parseStrikethrough.ts
+++ src/utilities/parseStrikethrough.ts
@@ -1,4 +1,40 @@
 export const parseStrikethrough = (line: string) => {
-  const strikethroughRegex = /~~(.*?)~~/g;
-  return line.replace(strikethroughRegex, "<del>$1</del>");
+  let newLine = "";
+  let charIndex = 0;
+  let isInsideStrikethrough = false;
+  let strikethroughText = "";
+
+  while (charIndex < line.length) {
+    if (
+      line[charIndex] === "~" &&
+      charIndex + 1 < line.length &&
+      line[charIndex + 1] === "~" &&
+      !isInsideStrikethrough
+    ) {
+      isInsideStrikethrough = true;
+      strikethroughText = "";
+      charIndex += 2;
+    } else if (
+      line[charIndex] === "~" &&
+      charIndex + 1 < line.length &&
+      line[charIndex + 1] === "~" &&
+      isInsideStrikethrough
+    ) {
+      isInsideStrikethrough = false;
+      charIndex += 2;
+      newLine += `<del>${strikethroughText}</del>`;
+    } else if (isInsideStrikethrough) {
+      strikethroughText += line[charIndex];
+      charIndex++;
+    } else {
+      newLine += line[charIndex];
+      charIndex++;
+    }
+  }
+
+  if (isInsideStrikethrough) {
+    newLine += "~~" + strikethroughText;
+  }
+
+  return newLine;
 };
