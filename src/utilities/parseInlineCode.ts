f44bddec4723aee716eb386b0f2137da98f9b975
〽️iraya <mirayaabrodi@gmail.com>
2024-01-17 17:31:14 +0100

    change: update parseInlineCode from regex to js

--- src/utilities/parseInlineCode.ts
+++ src/utilities/parseInlineCode.ts
@@ -1,4 +1,30 @@
 export const parseInlineCode = (line: string) => {
-  const inlineCodeRegex = /`(.*?)`/g;
-  return line.replace(inlineCodeRegex, "<code>$1</code>");
+  let newLine = "";
+  let charIndex = 0;
+  let isInsideCode = false;
+  let codeText = "";
+
+  while (charIndex < line.length) {
+    if (line[charIndex] === "`" && !isInsideCode) {
+      isInsideCode = true;
+      codeText = "";
+      charIndex++;
+    } else if (line[charIndex] === "`" && isInsideCode) {
+      isInsideCode = false;
+      charIndex++;
+      newLine += `<code>${codeText}</code>`;
+    } else if (isInsideCode) {
+      codeText += line[charIndex];
+      charIndex++;
+    } else {
+      newLine += line[charIndex];
+      charIndex++;
+    }
+  }
+
+  if (isInsideCode) {
+    newLine += "`" + codeText;
+  }
+
+  return newLine;
 };
