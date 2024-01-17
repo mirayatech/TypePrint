118391ec63ba7b837e0f291b334d5f743c96644f
〽️iraya <mirayaabrodi@gmail.com>
2024-01-17 17:35:19 +0100

    change: update parseHighlight from regex to js

--- src/utilities/parseHighlight.ts
+++ src/utilities/parseHighlight.ts
@@ -1,4 +1,40 @@
 export const parseHighlight = (line: string) => {
-  const highlightRegex = /==(.+?)==/g;
-  return line.replace(highlightRegex, "<span class='highlight'>$1</span>");
+  let newLine = "";
+  let charIndex = 0;
+  let isInsideHighlight = false;
+  let highlightText = "";
+
+  while (charIndex < line.length) {
+    if (
+      line[charIndex] === "=" &&
+      charIndex + 1 < line.length &&
+      line[charIndex + 1] === "=" &&
+      !isInsideHighlight
+    ) {
+      isInsideHighlight = true;
+      highlightText = "";
+      charIndex += 2;
+    } else if (
+      line[charIndex] === "=" &&
+      charIndex + 1 < line.length &&
+      line[charIndex + 1] === "=" &&
+      isInsideHighlight
+    ) {
+      isInsideHighlight = false;
+      charIndex += 2;
+      newLine += `<span class='highlight'>${highlightText}</span>`;
+    } else if (isInsideHighlight) {
+      highlightText += line[charIndex];
+      charIndex++;
+    } else {
+      newLine += line[charIndex];
+      charIndex++;
+    }
+  }
+
+  if (isInsideHighlight) {
+    newLine += "==" + highlightText;
+  }
+
+  return newLine;
 };
